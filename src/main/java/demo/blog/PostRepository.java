c2e360f551161242b7164974e3570c68f0fd3215
Josh Long <starbuxman@gmail.com>
2018-06-18 04:31:40 +0200

    library-ified

--- src/main/java/demo/blog/PostRepository.java
+++
@@ -1,9 +0,0 @@
-package demo.blog;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-/**
-	* @author <a href="mailto:josh@joshlong.com">Josh Long</a>
-	*/
-public interface PostRepository extends JpaRepository<Post, Long> {
-}
--- src/main/java/demo/crm/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package demo.crm;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-/**
-	* @author <a href="mailto:josh@joshlong.com">Josh Long</a>
-	*/
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
